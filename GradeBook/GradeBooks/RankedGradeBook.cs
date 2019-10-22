d7afcb6d8dd0522de661639baf8afe20e619885a
Marin Murgić <evoblue7@live.com>
2019-10-22 13:19:29 +0200

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -33,5 +33,24 @@ namespace GradeBook.GradeBooks
                 return 'F';
            
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else
+                base.CalculateStatistics();
+
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else
+                base.CalculateStudentStatistics(name);
+
+            
+        }
     }   
 }
